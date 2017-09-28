1401be84fcc7fb2f7589e4d3b327640344012b1f
connormcauliffe <cxm4176@rit.edu>
2017-09-28 17:11:17 -0400

    Got most of the new layout done. Just have to tweak the content

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 window.onload = function(){
-        var children = document.getElementById('projects').children;
+        var children = document.getElementById('left').querySelectorAll("#left > li")
         var indent = 10;
         for(var i = 0; i < children.length; i++){
                 children[i].style.paddingLeft = indent + "px";
                 indent +=20;
         }
-
 };
+
+function changeRight(id){
+        var rightDivs = document.getElementById('right').querySelectorAll("#right>div");
+
+        for(var i = 0; i < rightDivs.length; i++){
+
+                if(rightDivs[i].id == id){
+                        rightDivs[i].className = "show";
+                }
+                else{
+                        rightDivs[i].className = "hidden";
+                }
+        }
+        console.log('here');
+}
+
+
+function go_to_link(elem){
+        window.open("mailto:cxm4176@g.rit.edu");
+}
