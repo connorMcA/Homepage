e970ca79ddc861e25f56d16227fd91438c129d87
connormcauliffe <cxm4176@rit.edu>
2018-01-14 17:30:11 -0500

    finished initial version of QdUp project site

--- main.js
+++ main.js
@@ -20,6 +20,31 @@ function changeRight(id){
 }
 
 
-function go_to_link(elem){
-        window.open("mailto:cxm4176@g.rit.edu");
+function go_to_link(link){
+        if(link =='mail')
+                window.open("mailto:cxm4176@g.rit.edu");
+        else if (link == 'github')
+                window.open("https://github.com/WNM-Code/SpotiDJ")
+
+}
+
+
+function displayContent(id){
+        var backButton = document.getElementById('backButton');
+        if(id == 'main'){
+                backButton.className = "hidden";
+
+        }
+        else{
+                backButton.className = "show";
+        }
+        var contents = document.querySelectorAll('#phoneContents>div');
+        for(var i = 0; i < contents.length; i++){
+                if (contents[i].id == id){
+                        contents[i].className = "show";
+                }
+                else{
+                        contents[i].className = "hidden";
+                }
+        }
 }
